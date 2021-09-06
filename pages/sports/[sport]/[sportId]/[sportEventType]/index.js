f1271d0321f6b16ff1c0c8744694b2fdb3957ce5
Nabin Adhikari <nabin@stridesolutions.com.au>
2021-09-07 09:20:06 +1000

    added serversideprops

--- pages/sports/[sport]/[sportId]/[sportEventType]/index.js
+++ pages/sports/[sport]/[sportId]/[sportEventType]/index.js
@@ -1,14 +1,18 @@
 import { useRouter } from 'next/router';
 
 export default function Home() {
-    const router = useRouter();
-    const { sport, sportId, sportEventType } = router.query;
-    return (
-        <div>
-            <h1>Router test</h1>
-            {sport}
-            {sportId}
-            {sportEventType}
-        </div>
-    )
+  const router = useRouter();
+  const { sport, sportId, sportEventType } = router.query;
+  return (
+    <div>
+      <h1>Router test</h1>
+      {sport}
+      {sportId}
+      {sportEventType}
+    </div>
+  )
 }
+export const getServerSideProps = async ({ query }) => {
+  console.log('sport index:', query);
+  return { props: {} };
+};
--- pages/sports/[sport]/[sportId]/index.js
+++ pages/sports/[sport]/[sportId]/index.js
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router';
 
 export default function Home() {
-    const router = useRouter();
-    const { sport, sportId } = router.query;
-    return (
-        <div>
-            <h1>Router test</h1>
-            {sport}
-            {sportId}
-        </div>
-    )
+  const router = useRouter();
+  const { sport, sportId } = router.query;
+  return (
+    <div>
+      <h1>Router test</h1>
+      {sport}
+      {sportId}
+    </div>
+  )
 }
+export const getServerSideProps = async ({ query }) => {
+  console.log('sport index:', query);
+  return { props: {} };
+};
