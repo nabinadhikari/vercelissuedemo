db51e4c080cadeb67acf5cab3326bc44c53a9b88
Nabin Adhikari <nabin@stridesolutions.com.au>
2021-10-04 13:28:05 +1100

    cached dynamic

---
+++ pages/posts/[postId].js
@@ -0,0 +1,44 @@
+export default function PostDetail({ post, cachedNumber }) {
+    return (
+        <div>
+            <p>Cached for {cachedNumber} seconds</p>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </div>
+    )
+}
+export async function getStaticProps(context) {
+
+    const { postId } = context.params;
+    const post = await fetch('https://jsonplaceholder.typicode.com/posts/' + postId)
+        .then(r => r.json());
+    const cachedNumber = 100 - Number.parseInt(postId);
+
+    return {
+        props: {
+            post,
+            cachedNumber,
+        }, // will be passed to the page component as props
+        revalidate: cachedNumber,
+    };
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: 'blocking',
+    };
+}
+
+// export async function getServerSideProps(context) {
+//     // console.log(context);
+//     const { postId } = context.query;
+//     const post = await fetch('https://jsonplaceholder.typicode.com/posts/' + postId)
+//         .then(r => r.json());
+//     console.log(post)
+//     return {
+//         props: {
+//             post,
+//         }
+//     }
+// }
---
+++ pages/speedtest.js
@@ -0,0 +1,15 @@
+export default function Speedtest() {
+    return (
+        <div>
+            <h1>Speedtest</h1>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    return {
+        props: {
+
+        }
+    }
+}
