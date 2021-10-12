313c7118373118a5e025d70e3af8234e274b6234
Nabin Adhikari <nabin@stridesolutions.com.au>
2021-10-12 16:06:35 +1100

    LOG FOR INFO

--- pages/datecheck.js
+++ pages/datecheck.js
@@ -2,11 +2,14 @@ import moment from 'moment';
 export default function Date() {
     const cDate1 = "08May21";
     const date1 = moment(cDate1);
-    console.log({ date1, cDate1 })
+    console.log({ date1, cDate1 });
+    const DATE_FORMAT = 'DD MMM YY';
     return (
         <div>
             <h1>Date issue try</h1>
-            {cDate1} : {date1.format('DD MMM YY')}
+            {cDate1} : {date1.format(DATE_FORMAT)}
+            <br />
+            Current time {moment().format(DATE_FORMAT)}
         </div>
     );
 }
